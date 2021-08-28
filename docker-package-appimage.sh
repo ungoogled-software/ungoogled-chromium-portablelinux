2193394d32f4406b862221414c7124bf4215ec0d
Marco De Donno <mdedonno1337@gmail.com>
2021-08-28 21:11:34 +0200

    Add the FUSE library
    This is used to make the AppImage compilation. Since this is a kernel
    module, we install the `fuse` package to have the utilities. The
    `--privileged` option is here to be able to use the host kernel FUSE
    library (will not work otherwise).

--- docker-package-appimage.sh
+++ docker-package-appimage.sh
@@ -2,5 +2,5 @@ set -ex
 
 docker build --rm -f "Dockerfile-stage3" -t ug-chromium-builder-stage3:latest ./
 
-docker run -ti -v `pwd`:/data ug-chromium-builder-stage3:latest bash -c "./package.appimage.sh.ungoogin"
+docker run --privileged -ti -v `pwd`:/data ug-chromium-builder-stage3:latest bash -c "./package.appimage.sh.ungoogin"
 
