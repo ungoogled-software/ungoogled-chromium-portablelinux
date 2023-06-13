24c1dd015710be976997ee701f93a23159e28579
Clickot <clickot3@gmail.com>
2023-06-13 20:31:33 +0200

    override with state of clickot/ungoogled-chromium-build

--- build.sh
+++ build.sh
@@ -1,46 +1,73 @@
-#!/bin/bash -eux
+#!/bin/bash
 
-# Simple build script for Portable Linux
-# NOTE: This script will delete all build intermediates every time it runs. Proceed with caution.
+# directories
+# ==================================================
+root_dir=$(dirname $(readlink -f $0))
+download_cache="${root_dir}/target/download_cache"
+src_dir="${root_dir}/target/src"
+patches_dir="${root_dir}/patches"
+main_repo="${root_dir}/ungoogled-chromium"
 
-_root_dir=$(dirname $(readlink -f $0))
-_download_cache="$_root_dir/build/download_cache"
-_src_dir="$_root_dir/build/src"
-_main_repo="$_root_dir/ungoogled-chromium"
-
-rm -rf "$_src_dir" || true
-rm -f "$_root_dir/build/domsubcache.tar.gz" || true
-mkdir -p "$_src_dir/out/Default"
-mkdir -p "$_download_cache"
-
-"$_main_repo/utils/downloads.py" retrieve -i "$_main_repo/downloads.ini" -c "$_download_cache"
-"$_main_repo/utils/downloads.py" unpack -i "$_main_repo/downloads.ini" -c "$_download_cache" "$_src_dir"
-"$_main_repo/utils/prune_binaries.py" "$_src_dir" "$_main_repo/pruning.list"
-"$_main_repo/utils/patches.py" apply "$_src_dir" "$_main_repo/patches" "$_root_dir/patches"
-"$_main_repo/utils/domain_substitution.py" apply -r "$_main_repo/domain_regex.list" -f "$_main_repo/domain_substitution.list" -c "$_root_dir/build/domsubcache.tar.gz" "$_src_dir"
-cp "$_main_repo/flags.gn" "$_src_dir/out/Default/args.gn"
-cat "$_root_dir/flags.portable.gn" >> "$_src_dir/out/Default/args.gn"
-
-# Set commands or paths to LLVM-provided tools outside the script via 'export ...'
-# or before these lines
-export LLVM_VERSION=${LLVM_VERSION:=15}
+# env vars
+# ==================================================
+export LLVM_VERSION=${LLVM_VERSION:=16}
 export AR=${AR:=llvm-ar-${LLVM_VERSION}}
 export NM=${NM:=llvm-nm-${LLVM_VERSION}}
 export CC=${CC:=clang-${LLVM_VERSION}}
 export CXX=${CXX:=clang++-${LLVM_VERSION}}
 export LLVM_BIN=${LLVM_BIN:=/usr/lib/llvm-${LLVM_VERSION}/bin}
-# You may also set CFLAGS, CPPFLAGS, CXXFLAGS, and LDFLAGS
-# See build/toolchain/linux/unbundle/ in the Chromium source for more details.
+
+# clean
+# ==================================================
+echo "cleaning up directories"
+rm -rf "${src_dir}" "${root_dir}/target/domsubcache.tar.gz"
+mkdir -p "${src_dir}/out/Default" "${download_cache}"
+
+## fetch sources
+# ==================================================
+"${main_repo}/utils/downloads.py" retrieve -i "${main_repo}/downloads.ini" -c "${download_cache}"
+"${main_repo}/utils/downloads.py" unpack -i "${main_repo}/downloads.ini" -c "${download_cache}" "${src_dir}"
+
+# prepare sources 
+# ==================================================
+## apply own patches needed for build
+cd "${src_dir}"
+# revert addition of check for a certain llvm package version (seems to be introduced in chromium 107.xx)
+patch -Rp1 -i ${patches_dir}/REVERT-clang-version-check.patch
+# revert addition of compiler flag that needs newer clang (taken from ungoogled-chromium-archlinux)
+patch -Rp1 -i ${patches_dir}/REVERT-disable-autoupgrading-debug-info.patch
+# use the --oauth2-client-id= and --oauth2-client-secret= switches for setting GOOGLE_DEFAULT_CLIENT_ID 
+# and GOOGLE_DEFAULT_CLIENT_SECRET at runtime (taken from ungoogled-chromium-archlinux)
+patch -Np1 -i ${patches_dir}/use-oauth2-client-switches-as-default.patch
+# Disable kGlobalMediaControlsCastStartStop by default
+# https://crbug.com/1314342
+patch -Np1 -i ${patches_dir}/disable-GlobalMediaControlsCastStartStop.patch
+# VAAPI wayland support (taken from ungoogled-chromium-archlinux)
+patch -Np1 -i ${patches_dir}/ozone-add-va-api-support-to-wayland.patch
+# fix missing includes in av1_vaapi_video_encoder_delegate.cc
+patch -Np1 -i ${patches_dir}/av1_vaapi_video_encoder_delegate.patch
+
+
+## apply ungoogled-chromium patches
+"${main_repo}/utils/prune_binaries.py" "${src_dir}" "${main_repo}/pruning.list"
+"${main_repo}/utils/patches.py" apply "${src_dir}" "${main_repo}/patches"
+"${main_repo}/utils/domain_substitution.py" apply -r "${main_repo}/domain_regex.list" -f "${main_repo}/domain_substitution.list" -c "${root_dir}/target/domsubcache.tar.gz" "${src_dir}"
+
+## Link to system tools required by the build
+mkdir -p third_party/node/linux/node-linux-x64/bin && ln -s /usr/bin/node third_party/node/linux/node-linux-x64/bin
+
+### build
+# ==================================================
+## flags
+llvm_resource_dir=$("$CC" --print-resource-dir)
+export CXXFLAGS+=" -resource-dir=${llvm_resource_dir} -B${LLVM_BIN}"
+export CPPFLAGS+=" -resource-dir=${llvm_resource_dir} -B${LLVM_BIN}"
+export CFLAGS+=" -resource-dir=${llvm_resource_dir} -B${LLVM_BIN}"
 #
-# Hack to allow clang to find the default cfi_ignorelist.txt and LLVM tools
-# -B<prefix> defined here: https://clang.llvm.org/docs/ClangCommandLineReference.html
-_llvm_resource_dir=$("$CC" --print-resource-dir)
-export CXXFLAGS+="-resource-dir=${_llvm_resource_dir} -B${LLVM_BIN}"
-export CPPFLAGS+="-resource-dir=${_llvm_resource_dir} -B${LLVM_BIN}"
-export CFLAGS+="-resource-dir=${_llvm_resource_dir} -B${LLVM_BIN}"
+cat "${main_repo}/flags.gn" "${root_dir}/flags.gn" > "${src_dir}/out/Default/args.gn"
 
-cd "$_src_dir"
-
+## execute build
 ./tools/gn/bootstrap/bootstrap.py -o out/Default/gn --skip-generate-buildfiles
 ./out/Default/gn gen out/Default --fail-on-unused-args
+#
 ninja -C out/Default chrome chrome_sandbox chromedriver
--- docker-build.sh
+++ docker-build.sh
@@ -1,16 +1,38 @@
-set -ex
+#!/bin/bash
 
-git submodule update --init --recursive
 
-mkdir -p build
+BASE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
+GIT_REPO="ungoogled-chromium"
 
-# Build first stage docker
-pushd docker-stage1
-docker build --rm -f "Dockerfile" -t ug-chromium-builder-stage1:latest ./
-popd
+DISTRO_RELEASE=${1:-'debian:bookworm'}
+LLVM_VERSION=${2:-'16'}
+NODE_VERSION=${3:-'18'}
 
-# Build second stage docker
-docker build --rm -f "Dockerfile-stage2" -t ug-chromium-builder-stage2:latest ./
+DISTRO=$(echo ${DISTRO_RELEASE}| cut -d':' -f1)
+RELEASE=$(echo ${DISTRO_RELEASE}| cut -d':' -f2)
+[ "$LLVM_VERSION" -gt "16" ] || REPO_POSTFIX="-$LLVM_VERSION"
 
-# start the browser build
-docker run -ti -v `pwd`/build:/repo/build ug-chromium-builder-stage2:latest bash -c "./build.sh && ./package.sh"
+IMAGE="chromium-builder-${RELEASE}:llvm-${LLVM_VERSION}"
+
+cd $BASE_DIR 
+
+echo "docker buildx build -t ${IMAGE} --build-arg DISTRO=${DISTRO} --build-arg RELEASE=${RELEASE} --build-arg LLVM_VERSION=${LLVM_VERSION} --build-arg REPO_POSTFIX=${REPO_POSTFIX} --build-arg NODE_VERSION=${NODE_VERSION} ."
+(cd $BASE_DIR/docker && docker buildx build -t ${IMAGE} --build-arg DISTRO=${DISTRO} --build-arg RELEASE=${RELEASE} --build-arg LLVM_VERSION=${LLVM_VERSION} --build-arg REPO_POSTFIX=${REPO_POSTFIX} --build-arg NODE_VERSION=${NODE_VERSION} . )
+
+[ -n "$(ls -A ungoogled-chromium)" ] || git submodule update --init --recursive
+
+BUILD_START=$(date)
+echo "==============================================================="
+echo "  docker build start at ${BUILD_START}"
+echo "==============================================================="
+
+cd ${BASE_DIR}
+
+docker run -it -v ${BASE_DIR}:/repo ${IMAGE} /bin/bash -c "/repo/build.sh"
+
+BUILD_END=$(date)
+echo "==============================================================="
+echo "  docker build start at ${BUILD_START}"
+echo "  docker build end   at ${BUILD_END}"
+echo "==============================================================="
+
