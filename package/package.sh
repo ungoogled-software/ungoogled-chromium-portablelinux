862d066d751bdadb29cab50fd683cde4a6c7c9f0
Clickot <clickot3@gmail.com>
2025-07-07 18:02:59 +0200

    switch appimage creation to appimagetool to use the update info feature

--- package/package.sh
+++ package/package.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
 
 CURRENT_DIR=$(dirname $(readlink -f $0))
-
 ROOT_DIR=$(cd ${CURRENT_DIR}/.. && pwd)
 BUILD_DIR="${ROOT_DIR}/build"
+APP_DIR=${CURRENT_DIR}/ungoogled-chromium.AppDir
 
 chromium_version=$(cat ${ROOT_DIR}/ungoogled-chromium/chromium_version.txt)
 ungoogled_revision=$(cat ${ROOT_DIR}/ungoogled-chromium/revision.txt)
-
 FILE_PREFIX=ungoogled-chromium_${chromium_version}-${ungoogled_revision}
-ARCHIVE_OUTPUT="${CURRENT_DIR}/${FILE_PREFIX}_linux.tar.xz"
 
-set -eux
 
+### create tar.xz package
 FILES="chrome
 chrome_100_percent.pak
 chrome_200_percent.pak
@@ -35,25 +33,50 @@ vk_swiftshader_icd.json
 xdg-mime
 xdg-settings"
 
-
-mkdir ${CURRENT_DIR}/${FILE_PREFIX}_linux
+mkdir -p ${CURRENT_DIR}/${FILE_PREFIX}_linux
+set -x
 for i in $FILES ; do 
     cp -r ${BUILD_DIR}/src/out/Default/$i ${CURRENT_DIR}/${FILE_PREFIX}_linux
 done
 
 (cd ${CURRENT_DIR} && tar cf ${FILE_PREFIX}_linux.tar ${FILE_PREFIX}_linux)
 
-rm -rf ${CURRENT_DIR}/${FILE_PREFIX}_linux && xz "${CURRENT_DIR}/${FILE_PREFIX}_linux.tar" 
+rm -rf ${CURRENT_DIR}/${FILE_PREFIX}_linux ${CURRENT_DIR}/${FILE_PREFIX}_linux.tar.xz
+xz ${CURRENT_DIR}/${FILE_PREFIX}_linux.tar 
+set +x
 
-set +eux
+### create AppImage using appimagetool
+rm -rf ${APP_DIR} && mkdir -p ${APP_DIR}/opt/ungoogled-chromium/ ${APP_DIR}/usr/share/icons/hicolor/48x48/apps/
+tar xf ${CURRENT_DIR}/ungoogled-chromium_*_linux.tar.xz --strip-components=1 -C ${APP_DIR}/opt/ungoogled-chromium/
+cp ${CURRENT_DIR}/ungoogled-chromium.desktop ${APP_DIR}
+sed -i -e 's|Exec=chromium|Exec=AppRun|g' ${APP_DIR}/ungoogled-chromium.desktop
 
-if [ ! -f "./pkg2appimage" ] ; then
-    wget -c $(wget -q https://api.github.com/repos/AppImageCommunity/pkg2appimage/releases -O - | grep "pkg2appimage-.*-x86_64.AppImage" | grep browser_download_url | head -n 1 | cut -d '"' -f 4)
-    mv ./pkg2appimage-*.AppImage ./pkg2appimage && chmod +x ./pkg2appimage
+cat > ${APP_DIR}/AppRun <<'EOF'
+#!/bin/sh
+HERE="$(dirname "$(readlink -f "${0}")")"
+export LD_LIBRARY_PATH="${HERE}"/usr/lib:$PATH
+"${HERE}"/opt/ungoogled-chromium/chrome "$@"
+EOF
+chmod a+x ${APP_DIR}/AppRun
+
+cp ${APP_DIR}/opt/ungoogled-chromium/product_logo_48.png ${APP_DIR}/usr/share/icons/hicolor/48x48/apps/chromium.png
+cp ${APP_DIR}/usr/share/icons/hicolor/48x48/apps/chromium.png ${APP_DIR}
+# download appimagetool if not in PATH or locally present
+if ! command -v appimagetool >/dev/null; then
+    if [ ! -f ./appimagetool ] ; then
+        URL=$(curl -s https://api.github.com/repos/AppImage/appimagetool/releases/latest | jq '.assets[].browser_download_url' | grep x86_64 | sed 's/"//g')
+        wget -q --show-progress -O appimagetool $URL && chmod +x appimagetool
+    fi
+    export PATH=".:$PATH"
 fi
-./pkg2appimage ./ungoogled-chromium.yaml
+appimagetool -u 'gh-releases-zsync|ungoogled-software|ungoogled-chromium-portablelinux|latest|ungoogled-chromium_*.AppImage.zsync' ${APP_DIR}
 
-mv out/*.AppImage ${ROOT_DIR}/${FILE_PREFIX}.AppImage
-mv "${CURRENT_DIR}/${FILE_PREFIX}_linux.tar.xz" ${ROOT_DIR}
-rm -rf out ungoogled-chromium
+mv "Chromium_(ungoogled)-x86_64.AppImage" ${CURRENT_DIR}/${FILE_PREFIX}.AppImage
+mv "Chromium_(ungoogled)-x86_64.AppImage.zsync" ${CURRENT_DIR}/${FILE_PREFIX}.AppImage.zsync
+rm -rf ${APP_DIR}
+
+
+### mv results to root dir
+mv ${CURRENT_DIR}/${FILE_PREFIX}_linux.tar.xz "${ROOT_DIR}"
+mv ${CURRENT_DIR}/${FILE_PREFIX}.AppImage* "${ROOT_DIR}"
 
