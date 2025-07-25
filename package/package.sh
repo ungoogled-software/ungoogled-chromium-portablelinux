be9ce73947ad7e1915cea3d97dee7ba48e3286c5
wukko <me@wukko.me>
2025-07-25 18:14:59 +0600

    package: refactor

--- package/package.sh
+++ package/package.sh
@@ -13,7 +13,7 @@ VERSION="${chromium_version}-${ungoogled_revision}"
 ARCH="x86_64"
 FILE_PREFIX=$APP_NAME-$VERSION-$ARCH
 
-### create tar.xz package
+# Create tar.xz package
 FILES="chrome
 chrome_100_percent.pak
 chrome_200_percent.pak
@@ -36,16 +36,20 @@ vk_swiftshader_icd.json
 xdg-mime
 xdg-settings"
 
-echo "copying release files and create compressed archive ${FILE_PREFIX}_linux.tar.xz"
+echo "Copying release files and creating ${FILE_PREFIX}_linux.tar.xz"
+
 mkdir -p ${CURRENT_DIR}/${FILE_PREFIX}_linux
-for i in $FILES ; do 
+
+for i in $FILES ; do
     cp -r ${BUILD_DIR}/src/out/Default/$i ${CURRENT_DIR}/${FILE_PREFIX}_linux
 done
+
 SIZE="$(du -sk "${FILE_PREFIX}_linux" | cut -f1)"
 tar cf - ${FILE_PREFIX}_linux | pv -s"${SIZE}k" | xz > ${FILE_PREFIX}_linux.tar.xz
 
-## create AppImage using appimagetool
-rm -rf ${APP_DIR} && mkdir -p ${APP_DIR}/opt/ungoogled-chromium/ ${APP_DIR}/usr/share/icons/hicolor/48x48/apps/
+# Create AppImage
+rm -rf ${APP_DIR}
+mkdir -p ${APP_DIR}/opt/ungoogled-chromium/ ${APP_DIR}/usr/share/icons/hicolor/48x48/apps/
 mv ${CURRENT_DIR}/${FILE_PREFIX}_linux/* ${APP_DIR}/opt/ungoogled-chromium/
 cp ${CURRENT_DIR}/ungoogled-chromium.desktop ${APP_DIR}
 sed -i -e 's|Exec=chromium|Exec=AppRun|g' ${APP_DIR}/ungoogled-chromium.desktop
@@ -62,21 +66,21 @@ chmod a+x ${APP_DIR}/AppRun
 
 cp ${APP_DIR}/opt/ungoogled-chromium/product_logo_48.png ${APP_DIR}/usr/share/icons/hicolor/48x48/apps/chromium.png
 cp ${APP_DIR}/usr/share/icons/hicolor/48x48/apps/chromium.png ${APP_DIR}
-# download appimagetool if not in PATH or locally present
+
+# Download appimagetool if not in PATH
 if ! command -v appimagetool >/dev/null; then
     if [ ! -f ./appimagetool ] ; then
         URL=$(curl -s https://api.github.com/repos/AppImage/appimagetool/releases/latest | jq '.assets[].browser_download_url' | grep x86_64 | sed 's/"//g')
         wget -q --show-progress -O appimagetool $URL && chmod +x appimagetool
     fi
-    export PATH=".:$PATH"
+    export PATH="${CURRENT_DIR}:$PATH"
 fi
-APPIMAGETOOL_APP_NAME=$APP_NAME ARCH=$ARCH VERSION=$VERSION appimagetool -u 'gh-releases-zsync|ungoogled-software|ungoogled-chromium-portablelinux|latest|ungoogled-chromium-*.AppImage.zsync' ${APP_DIR}
 
-#mv "ungoogled-chromium-*-x86_64.AppImage" ${CURRENT_DIR}/${FILE_PREFIX}.AppImage
-#mv "ungoogled-chromium-*-x86_64.AppImage.zsync" ${CURRENT_DIR}/${FILE_PREFIX}.AppImage.zsync
+APPIMAGETOOL_APP_NAME=$APP_NAME ARCH=$ARCH VERSION=$VERSION appimagetool -u \
+    'gh-releases-zsync|ungoogled-software|ungoogled-chromium-portablelinux|latest|ungoogled-chromium-*.AppImage.zsync' \
+    ${APP_DIR}
+
 rm -rf ${CURRENT_DIR}/${FILE_PREFIX}_linux/ ${APP_DIR}
 
-### mv results to root dir
 mv ${CURRENT_DIR}/${FILE_PREFIX}_linux.tar.xz "${ROOT_DIR}"
 mv ${CURRENT_DIR}/${FILE_PREFIX}.AppImage* "${ROOT_DIR}"
-
